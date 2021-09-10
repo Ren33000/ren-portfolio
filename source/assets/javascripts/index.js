45fdcefadabcc91f8d5bd5a176d9bcc79463e225
Mylene Pont <pont.mylene@gmail.com>
2021-09-10 19:38:25 +0900

    cards on their way to be finished

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import Typed from 'typed.js';
 
 if (document.getElementById("animate")) {
-
+  
 new Typed('#animate', {
     strings: ["coder", "web-dev", "Front-End developper"],
     typeSpeed: 50,
