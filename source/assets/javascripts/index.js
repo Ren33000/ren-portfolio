63b987e425153ccf4d1bff602f00d372f29b53ec
Mylene Pont <pont.mylene@gmail.com>
2021-09-10 13:05:01 +0900

    typed.js added to the banner, navbar links fixed

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,12 @@
 import 'bootstrap';
+import Typed from 'typed.js';
+
+if (document.getElementById("animate")) {
+
+new Typed('#animate', {
+    strings: ["coder", "web-dev", "Front-End developper"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
