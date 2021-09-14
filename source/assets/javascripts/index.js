b40690cb2322aeee4f7d2d0cbdafbe8ae9261f1f
Mylene Pont <pont.mylene@gmail.com>
2021-09-14 21:39:33 +0900

    last changes before turn off

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -5,7 +5,7 @@ import AOS from 'aos';
 if (document.getElementById("animate")) {
   
 new Typed('#animate', {
-    strings: ["coder", "web-dev", "Front-End developper"],
+    strings: ["coder", "web-dev", "Front-End developer"],
     typeSpeed: 50,
     loop: true
   });
